8dca4b14ace6fa7c51ca409285ce770cc5002a03
HaloTester <mrttrkgl9090@gmail.com>
2021-12-31 14:08:30 +0100

    Added TC002 TC002 is done

---
+++ src/test/java/com/haloTester/tests/AlertPractice/TC002.java
@@ -0,0 +1,43 @@
+package com.haloTester.tests.AlertPractice;
+
+import com.haloTester.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC002 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test
+    public void TC002() {
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+
+        driver.findElement(By.xpath("//button[@id='enable-button']")).click();
+        WebElement enableButton = driver.findElement(By.xpath("//*[@*='disable']"));
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(enableButton));
+
+        boolean actualResult = enableButton.isEnabled();
+
+        Assert.assertTrue(actualResult,"verify that Button is enabled after 10 seconds");
+    }
+}
