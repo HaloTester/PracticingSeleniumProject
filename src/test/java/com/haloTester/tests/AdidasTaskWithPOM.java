c3faf3cff323b81c1bc9826045759c361a37b156
HaloTester <mrttrkgl9090@gmail.com>
2022-01-23 19:58:32 +0100

    Test cases added

---
+++ src/test/java/com/haloTester/tests/AdidasTaskWithPOM.java
@@ -0,0 +1,47 @@
+package com.haloTester.tests;
+
+import com.haloTester.pages.AdidasPages.CartPage;
+import com.haloTester.pages.AdidasPages.LoginPage;
+import com.haloTester.pages.AdidasPages.ProductPage;
+import com.haloTester.tests.TestBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AdidasTaskWithPOM extends TestBase {
+
+    int expectedPurchaseAmount = 0;
+    LoginPage loginPage = new LoginPage();
+    ProductPage productPage = new ProductPage();
+    CartPage cartPage = new CartPage();
+
+    @Test
+    public void loginTest(){
+        String expectedUsername = "asdfghjklöä";
+        loginPage.navigateTo(By.id("login2"));
+        loginPage.login();
+        System.out.println("loginPage.actualUsername.getText() = " + loginPage.actualUsername.getText());
+        Assert.assertTrue(loginPage.actualUsername.getText().contains(expectedUsername));
+    }
+
+
+    @Test
+    public void purchaseTest() throws InterruptedException {
+
+        expectedPurchaseAmount += productPage.productAdder("Laptops","Sony vaio i5");
+
+        expectedPurchaseAmount += productPage.productAdder("Laptops","Dell i7 8gb");
+
+        expectedPurchaseAmount -= productPage.productRemover("Dell i7 8gb");
+
+        productPage.navigateTo("Cart");
+
+        cartPage.placeOrderBtn.click();
+        cartPage.fillUpForm();
+        cartPage.purchaseBtn.click();
+        cartPage.logOfPurchase();
+        cartPage.okBtn.click();
+
+        Assert.assertEquals(cartPage.actualPurchaseAmount,expectedPurchaseAmount,"Verify that last amount is as expected");
+    }
+}
